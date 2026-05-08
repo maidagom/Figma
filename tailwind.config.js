f779aeeaf903acac0fa722b5857316fc966b5a2d
Maida Gómez <maidagompin@gmail.com>
2026-05-08 16:56:00 -0300

    Subiendo mini landing con figma

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["*.html"],
+        theme : {
+            extend : {
+                colors: { 
+                  Rosa: "#F195E0",
+                  borde: "#CD95E1"
+                },
+                    /*fontFamily : { 
+                      osw : ['', '']
+                    }*/
+
+            }
+        }
+}
